cca681b111361e00487849e553d7a841b9572241
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2021-06-28 15:48:33 -0400

    forgot argument in test

--- save.test.js
+++ save.test.js
@@ -2,7 +2,7 @@
 const save = require('./save');
 
 test('test save function', () => {
-    save({test: 'test'});
+    save({test: 'test'}, './test.json');
     const db = require('./test.json');
     expect(db).toEqual({test: 'test'});
 });
