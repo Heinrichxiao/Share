61517c880421408462664c942ca6c635b11ff37b
Heinrich-XIAO <74563446+Heinrich-XIAO@users.noreply.github.com>
2021-06-28 08:39:37 -0400

    Add app.js to run full program

--- index.js
+++ index.js
@@ -1,106 +1,108 @@
-require('dotenv').config();
+module.exports = () => {
+    require('dotenv').config();
 
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 8080;
-const path = require('path');
-const login = express.Router();
-const signup = express.Router();
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const hash = require('./hash');
-const fs = require('fs');
-const db = require('./db.json');
+    const express = require('express');
+    const app = express();
+    const port = process.env.PORT || 8080;
+    const path = require('path');
+    const login = express.Router();
+    const signup = express.Router();
+    const bodyParser = require('body-parser');
+    const session = require('express-session');
+    const hash = require('./hash');
+    const fs = require('fs');
+    const db = require('./db.json');
 
-app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, './views'));
-app.use(bodyParser());
-app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.SESSION_SECRET
-}));
+    app.set('view engine', 'pug');
+    app.set('views', path.join(__dirname, './views'));
+    app.use(bodyParser());
+    app.use(session({
+        resave: false,
+        saveUninitialized: false,
+        secret: process.env.SESSION_SECRET
+    }));
 
-login.get('/login', (req, res) => {
-    if (req.session.userid) {
-        res.redirect('/');
-        return;
-    }
-    res.render('login');
-    return;
-});
-
-login.post('/login', (req, res) => {
-    if (req.session.userid) {
-        res.redirect('/');
-        return;
-    }
-    console.log(db);
-    /* TODO: Authenticate */
-    if (req.body.usr == '') {
-        res.redirect('/login');
-    }
-    if (req.body.psw == '') {
-        res.redirect('/login');
-    }
-    if (req.body.usr in db.users && (!Object.keys(db.users).includes(req.body.usr))) {
-        res.redirect('/login');
-    }
-    if (Object.keys(db.users).includes(req.body.usr)) {
-        if (hash(req.body.psw, db.users[req.body.usr].psw.salt).hashed == db.users[req.body.usr].psw.hashed) {
-            req.session.userid = req.body.usr;
+    login.get('/login', (req, res) => {
+        if (req.session.userid) {
             res.redirect('/');
             return;
         }
-        console.log(hash(req.body.psw, db.users[req.body.usr].psw.salt));
-        console.log(db.users[req.body.usr].psw);
-    }
-    res.redirect('/login');
-});
-
-signup.get('/signup', (req, res) => {
-    if (req.session.userid) {
-        res.redirect('/');
+        res.render('login');
         return;
-    }
-    res.render('signup');
-    return;
-});
-
-signup.post('/signup', (req, res) => {
-    if (req.session.userid) {
-        res.redirect('/');
-        return;
-    }
-
-    if (req.body.usr == '') {
-        res.redirect('/signup');
-    }
-    if (req.body.psw == '') {
-        res.redirect('/signup');
-    }
-    if (Object.keys(db.users).includes(req.body.usr)) {
-        res.redirect('/login');
-    }
-    db.users[req.body.usr] = {
-        usr: req.body.usr,
-        psw: hash(req.body.psw)
-    };
-
-    fs.writeFile('./db.json', JSON.stringify(db), (err) => {
-        if (err) console.log(err);
     });
 
-    res.redirect('/login');
-});
+    login.post('/login', (req, res) => {
+        if (req.session.userid) {
+            res.redirect('/');
+            return;
+        }
+        console.log(db);
+        /* TODO: Authenticate */
+        if (req.body.usr == '') {
+            res.redirect('/login');
+        }
+        if (req.body.psw == '') {
+            res.redirect('/login');
+        }
+        if (req.body.usr in db.users && (!Object.keys(db.users).includes(req.body.usr))) {
+            res.redirect('/login');
+        }
+        if (Object.keys(db.users).includes(req.body.usr)) {
+            if (hash(req.body.psw, db.users[req.body.usr].psw.salt).hashed == db.users[req.body.usr].psw.hashed) {
+                req.session.userid = req.body.usr;
+                res.redirect('/');
+                return;
+            }
+            console.log(hash(req.body.psw, db.users[req.body.usr].psw.salt));
+            console.log(db.users[req.body.usr].psw);
+        }
+        res.redirect('/login');
+    });
 
-app.use(signup);
-app.use(login);
+    signup.get('/signup', (req, res) => {
+        if (req.session.userid) {
+            res.redirect('/');
+            return;
+        }
+        res.render('signup');
+        return;
+    });
 
-app.get('/*', (req, res) => {
-    res.render('404');
-});
+    signup.post('/signup', (req, res) => {
+        if (req.session.userid) {
+            res.redirect('/');
+            return;
+        }
 
-app.listen(port, () => {
-    console.log(`Starting server at: https://localhost:${port}`);
-});
+        if (req.body.usr == '') {
+            res.redirect('/signup');
+        }
+        if (req.body.psw == '') {
+            res.redirect('/signup');
+        }
+        if (Object.keys(db.users).includes(req.body.usr)) {
+            res.redirect('/login');
+        }
+        db.users[req.body.usr] = {
+            usr: req.body.usr,
+            psw: hash(req.body.psw)
+        };
+
+        fs.writeFile('./db.json', JSON.stringify(db), (err) => {
+            if (err) console.log(err);
+        });
+
+        res.redirect('/login');
+    });
+
+    app.use(signup);
+    app.use(login);
+
+    app.get('/*', (req, res) => {
+        res.render('404');
+    });
+
+    app.listen(port, () => {
+        console.log(`Starting server at: https://localhost:${port}`);
+    });
+};
